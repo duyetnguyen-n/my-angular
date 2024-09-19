05cd41e84fff764659acd39b818abbcae419d32d
Duyet <nguyentheduyet.mtp@gmail.com>
2024-09-19 15:58:39 +0700

    fix lại lỗi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './Pages/main/main.component';
-import { CriteriaComponent } from './Pages/criteria/criteria.component';
 
 export const routes: Routes = [
   {
